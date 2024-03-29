0466141034aea8a6c71df9b501ada0d763dacd0d
gomudayya <gomudayya@gmail.com>
2024-03-29 11:30:50 +0900

    [success] 전화번호 목록.java

---
+++ src/round05/전화번호 목록.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class Solution {
+    
+    Set<String> set = new HashSet<>();
+    
+    public boolean solution(String[] phone_book) {
+        set.addAll(Arrays.asList(phone_book));
+        
+        for (String now : phone_book) {
+            for (int i = 0; i < now.length(); i++) {
+                if (set.contains(now.substring(0,i))) {
+                    return false;
+                }
+            }
+
+            set.add(now);
+        }
+        return true;
+    }
+}
