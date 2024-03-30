c1838863090ce52c4bec7f6aaece3bf466db1877
thsamaji <60818655+thsamajiki@users.noreply.github.com>
2024-03-30 20:51:13 +0900

    [Success] 전화번호 목록 - 히데짱 (#15)

---
+++ src/round05/thsamajiki/전화번호_목록.java
@@ -0,0 +1,18 @@
+package round05.thsamajiki;
+
+import java.util.Arrays;
+
+public class 전화번호_목록 {
+    public boolean solution(String[] phone_book) {
+        boolean answer = true;
+
+        Arrays.sort(phone_book);
+        for(int i = 0; i < phone_book.length - 1; i++) {
+            if(phone_book[i + 1].startsWith(phone_book[i])) {
+                answer = false;
+            }
+        }
+
+        return answer;
+    }
+}
