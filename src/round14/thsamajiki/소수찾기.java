fd27cedaba29ee30c06841870a998a270b6ab70a
thsamaji <60818655+thsamajiki@users.noreply.github.com>
2024-05-07 18:21:35 +0900

    [Success] 소수찾기 - 히데짱

--- src/round14/thsamajiki/소수찾기.java
+++ src/round14/thsamajiki/소수찾기.java
@@ -1,7 +1,7 @@
 public class Solution {
     public int solution(int n) {
         int answer = 0;
-      
+      	
         for(int i = 2; i <= n; i++) {
             boolean num = true;
           
